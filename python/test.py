2ddead28d01be38dbb70e1400025a03b6e4454a1
1170300523 <2442536458@qq.com>
2019-03-17 08:28:47 +0800

    delete

--- python/test.py
+++ python/test.py
@@ -4,6 +4,7 @@ Created on Thu Feb  7 15:26:19 2019
 
 @author: lee32
 """
-import numpy as np
-a = np.arange(100).reshape(5,20)
-np.savetxt('test.csv',a,fmt = '%d',delimiter = ',')
+print('helloRRR')
+a = input()
+print(a)
+
